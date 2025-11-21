6861ac2250c0bedecaa559dd7dd314300000d018
Colm Austen O Sullivan <colm.austen@phorest.com>
2025-11-21 12:01:04 +0000

    Adding pay salon test data minus Stripe Key

---
+++ testData/colmPaySalonData.js
@@ -0,0 +1,23 @@
+export const testData = {
+  URL: {
+    BASE_URL: "https://my-dev.phorest.com",
+    API_URL: "http://dev.phorest.com",
+    API_GATEWAY_URL: "https://api-gateway-dev.phorest.com",
+    GRAPHQL_URL: "https://api-gateway-dev.phorest.com/api-facade/graphql",
+    TOKEN_URL: "https://api-gateway-dev.phorest.com/auth/oauth/token",
+  },
+  PAY_SALON: {
+    ACCOUNT_ID: 47403,
+    BUSINESS_ID: "x5Hz3_8G-PeOF3GeGcT-oQ",
+    BRANCH_ID: "h6x5cwGfZwHmyoHPpS9Qkw",
+    TERMINAL_ID: "tmr_F1ZwQgcHzZY0X5",
+    STRIPE_KEY: , //If running this locally, reach out to Colm/David to access the Stripe Secret Key required to use this salon
+    staff: [
+      {
+        name: "Rebecca Testerson",
+        email: "r.testerson@gmail.com",
+        id: "h6x5cwGfZwHmyoHPpS9Qkw",
+      },
+    ],
+  },
+};
