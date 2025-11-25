9778891467c31ce210157910937e7b384f1bc93b
Colm Austen O Sullivan <colm.austen@phorest.com>
2025-11-25 12:32:15 +0000

    Adding some basic sales screen validation

--- tests/integratedPurchase/cardPresentPayment.spec.js
+++ tests/integratedPurchase/cardPresentPayment.spec.js
@@ -1,15 +1,27 @@
 // @ts-nocheck
-import { test, expect, request  } from "@playwright/test";
+import { test, expect, request, page  } from "@playwright/test";
 import { loginLocators } from "../../locators/login/login.locators.js";
 import { paySlideoverLocators } from "../../locators/purchase_slideover.locators.js";
 import { testData } from "../../testData/colmPaySalonData.js";
 import generalCommands from "../../support/generalCommands.js";
 import { simulateVisaCardPresentment, simulateCardPresentment } from "../../support/stripe commands/cardPresentmentCommands.js";
+import purchasesRequests from "../../support/requests/purchases.requests.js";
 
 const staffEmail = testData.PAY_SALON.staff[0].email;;
 const staffPassword = process.env.staffPassword;
 const terminalId = testData.PAY_SALON.TERMINAL_ID;
-const stripeKey = testData.PAY_SALON.STRIPE_KEY
+const stripeKey = testData.PAY_SALON.STRIPE_KEY;
+
+// Helper function
+const getCurrentDate = () => {
+  return new Date(
+    new Date().getFullYear(),
+    new Date().getMonth(),
+    new Date().getDate()
+  )
+    .toJSON()
+    .slice(0, 10);
+};
 
 test("Process card present sale. @integratedPurchase", async ({ page, request }) => {
   await page.goto('/');
@@ -22,7 +34,8 @@ test("Process card present sale. @integratedPurchase", async ({ page, request })
     "a/" + testData.PAY_SALON.ACCOUNT_ID + "/appointments"
   );
   await page.getByRole('link', { name: 'Purchase' }).click();
-  await page.getByRole('button', { name: 'Walk in' }).click();
+  await page.getByPlaceholder('First name').fill('brant');
+  await page.getByRole('button', { name: 'BR Brant Rice' }).click();
   await page.getByRole('button', { name: 'JR Jamie Regressionson' }).click();
   await page.getByRole('button', { name: 'Services' }).click();
   await page.getByRole('button', { name: 'Massage' }).click();
@@ -33,14 +46,80 @@ test("Process card present sale. @integratedPurchase", async ({ page, request })
   await expect(page.getByText('Follow the instructions on the terminal')).toBeVisible();
   
   // Uses Stripe simulated terminal to simulate card presentment via Stripe API
-const paymentIntentId = await simulateVisaCardPresentment(request, {
+  const paymentIntentId = await simulateVisaCardPresentment(request, {
     terminalId: terminalId,
     stripeKey: stripeKey,
     retries: 5,
     interval: 1000
   });
 
- await expect(page.getByText('Sale complete!')).toBeVisible();
+  await expect(page.getByText('Sale complete!')).toBeVisible();
+  await page.locator(paySlideoverLocators.closePaymentButton).click();
+
+  const token = await generalCommands.getAccessToken(page);
+  const filterDate = String(getCurrentDate())
+
+  // Execute request to retrieve purchase data for today
+  const purchasesResponse = await purchasesRequests.retrieveMostRecentPurchase(request, token, filterDate);
+  
+  // Assert data has been returned
+  await expect(purchasesResponse.ok()).toBeTruthy();
+  await expect(purchasesResponse.status()).toBe(200);
+
+  // Purchase data extraction to be used for validations (just commented out logging to avoid congestion in console)
+  const todaysPurchases = await purchasesResponse.json();
+  // console.log(todaysPurchases);
+  const mostRecentPurchase = await todaysPurchases.data.purchases.edges[0].node
+  // console.log(mostRecentPurchase);
+  const purchaseTotal = await Number(mostRecentPurchase.totalAmount)
+  const purchaseClient = await mostRecentPurchase.clientName
+  const paymentMethodCode = await mostRecentPurchase.payments[0].code
+  const paymentMethodAmount = await Number(mostRecentPurchase.payments[0].amount)
+  const paymentMethodTransactionId = await mostRecentPurchase.payments[0].cardTransactions[0].transactionId
+  
+  // Printing required validation data to console
+  console.log(purchaseTotal);
+  console.log(purchaseClient);
+  console.log(paymentMethodCode);
+  console.log(paymentMethodAmount);
+  console.log(paymentMethodTransactionId);
+
+  // Validate sales screen data - taken from 
+  await page.getByRole('link', { name: 'Manager' }).click();
+  await page.locator("#sales").click();
+  const table = page.locator('table');
+  await expect(table).toBeVisible(); // Putting await so that table validation doesn't fire prior to table actually being visible
+  const headers = table.locator('thead tr th');
+
+  const headerCount = await headers.count();
+  let paymentIndex = -1;
+
+  for (let i = 0; i < headerCount; i++) {
+    const text = (await headers.nth(i).innerText()).trim();
+    if (text === 'Payment') {
+      paymentIndex = i;
+      break;
+    }
+  };
+
+  if (paymentIndex === -1) {
+    throw new Error('Could not find "Payment" column in the table header');
+  };
+
+  const clientRow = table.locator('tbody tr', {
+    has: page.locator('td', { hasText: purchaseClient }),
+  });
+
+  const paymentText = (await clientRow.locator('td').nth(paymentIndex).innerText()).trim();
+  const expected = `${paymentMethodCode}(${Number(paymentMethodAmount).toFixed(2)})`;
+  console.log(paymentText);
+  console.log(expected);
+
+  await expect(paymentText).toBe(expected);
+
+  if (paymentText === expected) {
+    console.log('Sales screen entry successfully validated for this payment');;
+  };
 
   // Query the payment intent via Stripe API
   const paymentIntentResponse = await request.get(
@@ -59,4 +138,6 @@ const paymentIntentId = await simulateVisaCardPresentment(request, {
   const paymentIntentResponseBody = await paymentIntentResponse.json();
   const paymentIntentStatus = paymentIntentResponseBody.status;
   expect(paymentIntentStatus).toBe('succeeded');
+
+  await expect(paymentMethodTransactionId).toEqual(paymentIntentId)
 })
