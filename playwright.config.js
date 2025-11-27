847594291eb978a43e9fab12c1356290cd4bb43e
Kohil Namdeo <kohilnamdeo@gmail.com>
2025-11-27 16:43:59 +0000

    updated to correct test directory

--- playwright.config.js
+++ playwright.config.js
@@ -16,7 +16,7 @@ dotenv.config();
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './tests/marketing',
+  testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
