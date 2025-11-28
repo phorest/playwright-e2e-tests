ae2faee384045655b173db628beb8d917ae5d080
Colm Austen O Sullivan <colm.austen@phorest.com>
2025-11-28 15:16:50 +0000

    Nesting if statements to reduce console printing

--- support/stripe commands/virtualTerminalCommands.js
+++ support/stripe commands/virtualTerminalCommands.js
@@ -18,8 +18,6 @@ export async function submitVirtualTerminalCardDetails(page, {
   await cvcEntry.type(cvcNumber);
   const countryDropdown = stripeFrame.locator(paySlideoverLocators.countryInputDropdown);
   const zipCodeEntry = stripeFrame.locator(paySlideoverLocators.postalCodeInputField)
-  // This step is hard setting the payment country to avoid needing a postal code > new if clause should resolve this
-  // await countryDropdown.selectOption('Puerto Rico');
 
   // Checks to ensure a post code is not required to a post code is required for the payment
   const countryValue = await countryDropdown.inputValue();
@@ -28,28 +26,22 @@ export async function submitVirtualTerminalCardDetails(page, {
     console.log('US ZIP Code required');
     await zipCodeEntry.type('10009')
     } else {
-    console.log('US ZIP code not required');
-  }
-
-    if (countryValue === 'GB') {
-    console.log('UK Postcode required');
-    await zipCodeEntry.type('EC1Y 8SY')
-    } else {
-    console.log('UK Postcode not required');
-  }
-
-  if (countryValue === 'CA') {
-    console.log('Canadian post code required');
-    await zipCodeEntry.type('M5H 2N2')
-    } else {
-    console.log('Canadian post code not required');
-  }
-
-    if (countryValue === 'PR') {
-    console.log('Puerto Rican post code required');
-    await zipCodeEntry.type('00631')
-    } else {
-    console.log('Puerto Rican post code not required');
+        if (countryValue === 'GB') {
+        console.log('UK Postcode required');
+        await zipCodeEntry.type('EC1Y 8SY')
+        } else {
+            if (countryValue === 'CA') {
+            console.log('Canadian post code required');
+            await zipCodeEntry.type('M5H 2N2')
+            } else {
+                if (countryValue === 'PR') {
+                console.log('Puerto Rican post code required');
+                await zipCodeEntry.type('00631')
+                } else {
+                console.log('Post code not required for this country');
+                }
+            }
+        }
   }
   
   console.log(`💳 ${paymentScenario} card details successfully entered`);
